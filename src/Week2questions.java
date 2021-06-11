421ef0281a7ee895a191dfd60d38f8ef50ac68bb
Candice <candicehenderson1215@gmail.com>
2021-06-11 14:18:21 -0700

    First Commit

---
+++ src/Week2questions.java
@@ -0,0 +1,13 @@
+
+public class Week2questions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+        
+		System.out.println("Hello World!");
+        
+		
+	}
+
+}
